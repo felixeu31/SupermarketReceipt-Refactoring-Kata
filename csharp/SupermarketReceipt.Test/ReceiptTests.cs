6d222ef9e1b68f930f4a6f43ffae23de1e52cc25
Félix Díez <felixeu31@gmail.com>
2022-12-18 09:53:42 +0100

    test: total price with products and discounts

--- csharp/SupermarketReceipt.Test/ReceiptTests.cs
+++ csharp/SupermarketReceipt.Test/ReceiptTests.cs
@@ -24,23 +24,6 @@ public class ReceiptTests
         receiptItems.Should().Contain(x => x.Product.Name == "product2");
     }
 
-
-    [Fact]
-    public void GetItems_CalculateTotalPrice_WhenAddProducts()
-    {
-        // Arrange
-        Receipt receipt = new Receipt();
-        Product product1 = new Product("product1", ProductUnit.Each);
-        Product product2 = new Product("product2", ProductUnit.Each);
-
-        // Act
-        receipt.AddProduct(product1, 1, 1, 1);
-        receipt.AddProduct(product2, 1, 1, 1);
-
-        // Assert
-        receipt.GetTotalPrice().Should().Be(2);
-    }
-
     [Fact]
     public void GetDiscount_ReturnAddedDiscounts()
     {
@@ -62,5 +45,41 @@ public class ReceiptTests
         receiptDiscounts.Should().HaveCount(2);
         receiptDiscounts.Should().Contain(x => x.Description == "discount1");
         receiptDiscounts.Should().Contain(x => x.Description == "discount2");
+    }
+    
+    [Fact]
+    public void GetTotalPrice_WhenAddProducts()
+    {
+        // Arrange
+        Receipt receipt = new Receipt();
+        Product product1 = new Product("product1", ProductUnit.Each);
+        Product product2 = new Product("product2", ProductUnit.Each);
+
+        // Act
+        receipt.AddProduct(product1, 1, 1, 1);
+        receipt.AddProduct(product2, 1, 1, 1);
+
+        // Assert
+        receipt.GetTotalPrice().Should().Be(2);
+    }
+
+    [Fact]
+    public void GetTotalPrice_WhenAddProductsAndDiscounts()
+    {
+        // Arrange
+        Receipt receipt = new Receipt();
+        Product product1 = new Product("product1", ProductUnit.Each);
+        Product product2 = new Product("product2", ProductUnit.Each);
+        Discount discount1 = new Discount(product1, "discount1", -0.2);
+        Discount discount2 = new Discount(product1, "discount2", -0.2);
+
+        // Act
+        receipt.AddProduct(product1, 1, 1, 1);
+        receipt.AddProduct(product2, 1, 1, 1);
+        receipt.AddDiscount(discount1);
+        receipt.AddDiscount(discount2);
+
+        // Assert
+        receipt.GetTotalPrice().Should().Be(1.6);
     }
 }
