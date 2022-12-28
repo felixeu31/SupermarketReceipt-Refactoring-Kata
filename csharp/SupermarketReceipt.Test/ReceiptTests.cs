a7636552173db530d1a9a3b48e17a925688f6f5a
Félix Díez <felixeu31@gmail.com>
2022-12-28 12:59:01 +0100

    test: receipt calculates from quantity and price

--- csharp/SupermarketReceipt.Test/ReceiptTests.cs
+++ csharp/SupermarketReceipt.Test/ReceiptTests.cs
@@ -64,22 +64,40 @@ public class ReceiptTests
     }
 
     [Fact]
+    public void GetTotalPrice_CalculatesFromQuantityAndPrice()
+    {
+        // Arrange
+        Receipt receipt = new Receipt();
+        Product product1 = new Product("product1", ProductUnit.Each);
+        Product product2 = new Product("product2", ProductUnit.Each);
+
+        // Act
+        receipt.AddProduct(product1, 2, 1, 1);
+        receipt.AddProduct(product2, 1, 2, 1);
+
+        // Assert
+        receipt.GetTotalPrice().Should().Be(4);
+    }
+
+    [Fact]
     public void GetTotalPrice_WhenAddProductsAndDiscounts()
     {
         // Arrange
         Receipt receipt = new Receipt();
         Product product1 = new Product("product1", ProductUnit.Each);
+        Product repeatedProduct1 = new Product("product1", ProductUnit.Each);
         Product product2 = new Product("product2", ProductUnit.Each);
         Discount discount1 = new Discount(product1, "discount1", -0.2);
         Discount discount2 = new Discount(product1, "discount2", -0.2);
 
         // Act
         receipt.AddProduct(product1, 1, 1, 1);
+        receipt.AddProduct(repeatedProduct1, 1, 1, 1);
         receipt.AddProduct(product2, 1, 1, 1);
         receipt.AddDiscount(discount1);
         receipt.AddDiscount(discount2);
 
         // Assert
-        receipt.GetTotalPrice().Should().Be(1.6);
+        receipt.GetTotalPrice().Should().Be(2.6);
     }
 }
--- csharp/SupermarketReceipt/Receipt.cs
+++ csharp/SupermarketReceipt/Receipt.cs
@@ -10,7 +10,7 @@ namespace SupermarketReceipt
 
         public double GetTotalPrice()
         {
-            return _items.Sum(x => x.TotalPrice)
+            return _items.Sum(x => x.Quantity * x.Price)
                    + _discounts.Sum(x => x.DiscountAmount);
         }
 
