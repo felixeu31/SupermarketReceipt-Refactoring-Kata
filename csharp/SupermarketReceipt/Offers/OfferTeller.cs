1dbe36932082598b897cad001a231e333e7fd651
Félix Díez <felixeu31@gmail.com>
2023-01-06 22:33:41 +0100

    refactor: invert offer teller iteration

--- csharp/SupermarketReceipt/Offers/OfferTeller.cs
+++ csharp/SupermarketReceipt/Offers/OfferTeller.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using SupermarketReceipt.Offers.OfferCalculators;
 using SupermarketReceipt.Products;
 using SupermarketReceipt.Receipts;
@@ -12,22 +13,22 @@ public class OfferTeller
     // How can I test this class. It has a collaborator instantiated by a factory.
     // Do i want to test this class without the collaborator taking place.
     // Does it mean I have a bad design for this class??
+    // Should i use it as a pitch point (Working effectively with legacy code)
     public static List<Discount> CalculateDiscounts(Dictionary<Product, Offer> offers, SupermarketCatalog catalog, Dictionary<Product, double> productQuantities)
     {
         List<Discount> discounts = new List<Discount>();
 
-        foreach (var product in productQuantities.Keys)
+        foreach (var offer in offers)
         {
-            if (!offers.ContainsKey(product))
+            if (!productQuantities.ContainsKey(offer.Key))
             {
                 continue;
             }
 
-            var quantity = (int)productQuantities[product];
-            var offer = offers[product];
-            var unitPrice = catalog.GetUnitPrice(product);
+            var quantity = (int)productQuantities[offer.Key];
+            var unitPrice = catalog.GetUnitPrice(offer.Key);
 
-            var offerCalculator = OfferCalculatorFactory.CreateOfferCalculator(offer);
+            var offerCalculator = OfferCalculatorFactory.CreateOfferCalculator(offer.Value);
 
             var discount = offerCalculator.CalculateDiscount(quantity, unitPrice);
 
